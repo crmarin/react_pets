ba8f742b37f7708077bb8d1822eb3bb4155efb58
cristian <tecnologia2@movimientomira.net>
2020-03-30 14:36:05 -0500

    setting state with componentDidMount

--- main.js
+++ main.js
@@ -4,49 +4,40 @@ var ReactDOM = require('react-dom');
 var MainInterface = React.createClass({
     getInitialState: function () {
         return {
-            data: [
-                {
-                    "petName": "Buffy",
-                    "ownerName": "Hassum Harrod",
-                    "aptDate": "2016-06-20 15:30",
-                    "aptNotes": "This Chihuahua has not eaten for three days and is lethargic"
-                },
-                {
-                    "petName": "Spot",
-                    "ownerName": "Constance Smith",
-                    "aptDate": "2016-06-24 08:30",
-                    "aptNotes": "This German Shepherd is having some back pain"
-                },
-                {
-                    "petName": "Goldie",
-                    "ownerName": "Barot Bellingham",
-                    "aptDate": "2016-06-22 15:50",
-                    "aptNotes": "This Goldfish has some weird spots in the belly"
-                },
-                {
-                    "petName": "Mitten",
-                    "ownerName": "Hillary Goldwyn",
-                    "aptDate": "2016-06-21 9:15",
-                    "aptNotes": "Cat has excessive hairballs"
-                }
-            ]
+            myAppointmets: []
         }
     },
+
+    componentDidMount() {
+        this.serverRequest = $.get('./data/data.json', function (result) {
+            var tempApts = result;
+
+            this.setState({
+                myAppointmets: tempApts
+            });
+
+        }.bind(this));
+    },
+
+    componentWillUnmount() {
+        this.serverRequest.abort();
+    },
+
     render: function () {
-        var filteredApts = this.state.data;
+        var filteredApts = this.state.myAppointmets;
         filteredApts = filteredApts.map(function (item, index) {
             return (
                 <li className="pet-item media" key={index}>
                     <div className="pet-info media-body">
                         <div className="pet-head">
-                            <span className="pet-name">{this.state.data[index].petName}</span>
-                            <span className="apt-date pull-right">{this.state.data[index].aptDate}</span>
+                            <span className="pet-name">{this.state.myAppointmets[index].petName}</span>
+                            <span className="apt-date pull-right">{this.state.myAppointmets[index].aptDate}</span>
                         </div>
                         <div className="owner-name">
                             <span className="label-item">Owner:</span>
-                            {this.state.data[index].ownerName}
+                            {this.state.myAppointmets[index].ownerName}
                         </div>
-                        <div className="apt-notes">{this.state.data[index].aptNotes}
+                        <div className="apt-notes">{this.state.myAppointmets[index].aptNotes}
                         </div>
                     </div>
                 </li>
