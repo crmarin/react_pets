097c447def758467f048659f4edadb99125f5f3b
tecnologia2mm <tecnologia2@movimientomira.net>
2020-05-05 20:34:49 -0500

    inital config and getAllTodos

---
+++ functions/index.js
@@ -0,0 +1,9 @@
+const functions = require('firebase-functions');
+const app = require('express')();
+
+const {
+    getAllTodos
+} = require('./APIs/todos')
+
+app.get('/todos', getAllTodos);
+exports.api = functions.https.onRequest(app);
